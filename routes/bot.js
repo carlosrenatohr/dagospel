a210b4aee8f571e54c3d2914e287b8301d9e0014
Carlos R. Hernandez <honchkrow1995@gmail.com>
2018-02-11 11:39:52 -0600

    Responses added for deploy flags

--- routes/bot.js
+++ routes/bot.js
@@ -4,12 +4,12 @@ const Telegraf = require('telegraf');
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 router.get('/', function (req, res, next) {
-    bot.start((ctx) => {
+      bot.start((ctx) => {
         console.log('started:', ctx.from.id)
         return ctx.reply('Welcome!')
       })
       
-      bot.command('help', (ctx) => ctx.reply('Try send a sticker!'))
+      bot.command('help', (ctx) => {console.log('user requires help', ctx);ctx.reply('Try send a sticker!')})
       bot.hears('hi', (ctx) => {console.log('ctx id after hi:', ctx); ctx.reply('Hey there!')})
       bot.hears(/buy/i, (ctx) => ctx.reply('Buy-buy!'))
       bot.on('sticker', (ctx) => ctx.reply('👍'))
