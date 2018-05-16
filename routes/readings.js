208552eb8a9ab8f2582e21957216187073133309
Carlos R. Hernandez <honchkrow1995@gmail.com>
2018-05-16 00:33:18 -0600

    `Change request by AXIOS
    - add package and change of logic
    `

--- routes/readings.js
+++ routes/readings.js
@@ -3,16 +3,17 @@ const router = express.Router();
 const cheerio = require('cheerio');
 const moment = require('moment');
 const http = require('http');
+const axios = require('axios');
 const reading = require('../models/Reading');
 
 /* GET reading data. */
 router.get('/', function (req, res, next) {
   var promises = [];
   var start_date = '2018-01-01';
-  var end_date = '2018-01-05';
+  var end_date = '2018-01-31';
   var path = '/calendario-lecturas/evangelio-del-dia/?f=';
   var options = {
-    host: 'www.ciudadredonda.org',
+    host: 'https://www.ciudadredonda.org',
     path: ''// ?f=2018-12-31
   }
 
@@ -20,17 +21,13 @@ router.get('/', function (req, res, next) {
   var objs = [];
 
   while (start_date != end_date) {
-    console.log(start_date);
     var funct = new Promise(function (resolve, reject) {
-      var data = '';
+      //var data = '';
       var this_date = start_date;
       options.path = path + this_date;
-      var request = http.request(options, function (resp) {
-        resp.on('data', function (chunk) {
-          data += chunk;
-        });
-
-        resp.on('end', function () {
+      var request = axios.get(options.host + path + this_date)
+        .then(function(resp){
+          var data = resp.data;
           console.log('Ciudad Redonda Content:'); //console.log(data);
           var $ = cheerio.load(data);
 
@@ -54,6 +51,47 @@ router.get('/', function (req, res, next) {
               content: $(el).find(content_class).clone().children().remove().end().text(),
               ending: $(el).find('.texto_palabra b').last().text(),
             };
+            console.log(new_reading);
+            objs[i] = new_reading;
+            //console.log(new_reading);
+            //todo SET an function to create/update readings
+            reading.create(new_reading);
+            data = '';
+          });
+        })
+        .catch(function(e) {
+          console.log(e.message);
+        })
+      /*var request = http.request(options, function (resp) {
+        resp.on('data', function (chunk) {
+          data += chunk;
+        });
+
+        resp.on('end', function () {
+          console.log('Ciudad Redonda Content:'); console.log(data);
+          var $ = cheerio.load(data);
+
+          var lecturas = $('.lecturas section');
+          var dayname = $('.ar_titulo').text();
+          var dayval = $('.subtitulo time').attr('datetime');
+          console.log('lecturas del dia')
+          lecturas.each(function (i, el) {
+            if ($(el).find('.texto_evangelio').length > 0) {
+              var content_class = '.texto_evangelio';
+            } else {
+              var content_class = '.texto_palabra';
+            }
+
+            var new_reading = {
+              dayname: dayname,
+              dayval: dayval,
+              title: $(el).find('h2').html(),
+              quote: $(el).find('.texto_palabra b').first().text(),
+              date: new Date(moment(this_date).format()).toISOString(),
+              content: $(el).find(content_class).clone().children().remove().end().text(),
+              ending: $(el).find('.texto_palabra b').last().text(),
+            };
+            console.log(new_reading);
             objs[i] = new_reading;
             //console.log(new_reading);
             //todo SET an function to create/update readings
@@ -68,7 +106,7 @@ router.get('/', function (req, res, next) {
         console.log(e.message);
       });
 
-      request.end();
+      request.end();*/
       start_date = moment(start_date).add(1, 'days').format('YYYY-MM-DD');
       resolve(start_date);
     });
